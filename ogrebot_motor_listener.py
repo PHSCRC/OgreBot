b254233b656ad8d08ac588c04f8c81db48e351b2
Lincoln Roth <lincolnmroth@gmail.com>
2019-03-20 12:13:54 -0400

    puts vel listener back into motor listener

--- ogrebot_motor_listener.py
+++ ogrebot_motor_listener.py
@@ -15,6 +15,10 @@ WHEEL_RADIUS = .045 # in meters
 ENCODER_COUNTS_PER_RADIAN = 1303.7972938088 #(2048 * 4)/(2*math.pi)
 
 POLL_TIME=0.01
+
+isInPositionMode=False
+targetPos=0
+
 def callback(cmd_vel):
     global my_drive, vels
 #    print("linearx", cmd_vel.linear.x)
@@ -49,7 +53,7 @@ def turn(rad):
     my_drive.axis1.controller.config.vel_limit = speed
 
 def drive(distance):
-    global my_drive
+    global my_drive, targetPos
     my_drive.axis0.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
     my_drive.axis1.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
     speed = my_drive.axis0.controller.config.vel_limit
@@ -58,15 +62,29 @@ def drive(distance):
     countsToMove=(distance.data * ENCODER_COUNTS_PER_RADIAN)/WHEEL_RADIUS
     print("distance", distance.data)
     print("counts", countsToMove)
+    targetPos=countsToMove+my_drive.axis0.encoder.pos_estimate
     my_drive.axis0.controller.pos_setpoint = my_drive.axis0.encoder.pos_estimate - countsToMove
     my_drive.axis1.controller.pos_setpoint = my_drive.axis1.encoder.pos_estimate +  countsToMove
     time.sleep(10)
    # while (my_drive.axis0.encoder.vel_estimate < 100):
    #     pass
-    my_drive.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
-    my_drive.axis1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
-    my_drive.axis0.controller.config.vel_limit = speed
-    my_drive.axis1.controller.config.vel_limit = speed
+
+def poll():
+    global my_drive, isInPositionMode, targetPos, vels
+    if isInPositionMode:
+        if(targetPos-my_drive.axis0.controller.pos_estimate<100):
+            my_drive.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+            my_drive.axis1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+            my_drive.axis0.controller.config.vel_limit = speed
+            my_drive.axis1.controller.config.vel_limit = speed
+    leftReading = my_drive.axis0.encoder.vel_estimate/ENCODER_COUNTS_PER_RADIAN
+    rightReading = my_drive.axis1.encoder.vel_estimate/ENCODER_COUNTS_PER_RADIAN
+    msg = robot_vels()
+    msg.left_vel = leftReading
+    msg.right_vel = rightReading
+    msg.POLL_TIME = .01
+    msg.ROBOT_RADIUS = ROBOT_RADIUS
+    vels.publish(msg)
 
 def listener():
     global my_drive, vels
@@ -81,6 +99,8 @@ def listener():
     my_drive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
     my_drive.axis1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
 
+
+
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
@@ -88,7 +108,8 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=False)
     rospy.Subscriber("/cmd_vel", Twist, callback)
-
+    vels = rospy.Publisher('wheel_vels', robot_vels, queue_size=10)
+    rospy.Timer(rospy.Duration(POLL_TIME), poll, oneshot=False)
     rospy.Subscriber("/turn", Float64, turn)
     rospy.Subscriber("/drive", Float64, drive)
 
